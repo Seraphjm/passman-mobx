e87143b71a897286c91afa68c29d3d1d08e678e6
Zhilin Konstantin <seraphjm@yandex.ru>
2021-01-18 00:03:22 +0300

    added settings template

--- src/Modules/Settings/Pages/Settings.tsx
+++ src/Modules/Settings/Pages/Settings.tsx
@@ -1,15 +1,33 @@
 import {FunctionComponent} from 'react';
 import {Link} from 'react-router-dom';
+import {RadioButton, RadioSelectButton} from 'ui';
 import {AUTH_ROUTE_NAMES} from 'Modules/Auth/Router/Routes';
+import {MAIN_ROUTE_NAMES} from 'Modules/Main/Router/Routes';
+import {ELanguage} from '../Enums';
+import {useSettings} from '../Store/Hooks';
+import './Settings.style.scss';
 
 /**
- * TODO: в разработке.
+ * Страница настроек приложения.
  */
-const Settings: FunctionComponent = () => (
-    <div>
-        <Link to={AUTH_ROUTE_NAMES.ROOT}>GO TO Authorization</Link>
-        <div>SETTINGS</div>
-    </div>
-);
+const Settings: FunctionComponent = () => {
+    const settings = useSettings();
+    return (
+        <div className="settings">
+            <div className="settings__sidebar">
+                <Link to={AUTH_ROUTE_NAMES.ROOT}>GO TO Authorization</Link>
+                <br />
+                <br />
+                <Link to={MAIN_ROUTE_NAMES.ROOT}>GO TO MAIN</Link>
+            </div>
+            <div>
+                <RadioSelectButton onChange={settings.setLanguage} value={settings.language}>
+                    <RadioButton value={ELanguage.en_US}>English</RadioButton>
+                    <RadioButton value={ELanguage.ru_RU}>Русский</RadioButton>
+                </RadioSelectButton>
+            </div>
+        </div>
+    );
+};
 
 export default Settings;
